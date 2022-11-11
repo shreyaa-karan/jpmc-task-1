eb6dfe368df2ddc22bf7d9d225ed1018d87065c8
shreyaa-karan <karan@purdue.edu>
2022-11-11 11:33:17 -0500

    changes made to both classes

--- client.py
+++ client.py
@@ -38,7 +38,6 @@ def getDataPoint(quote):
     price = (bid_price+ask_price)/2
     return stock, bid_price, ask_price, price
 
-
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
